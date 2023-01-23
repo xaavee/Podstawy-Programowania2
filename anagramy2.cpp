07b234e41154dc822100f1cbcf7ed6693ccc026b
xaavee <deefikyt@gmail.com>
2023-01-23 08:15:18 +0100

    s

--- anagramy2.cpp
+++ anagramy2.cpp
@@ -5,24 +5,27 @@ using namespace std;
 //sa anagramami
 
 int main() {
-	string wyraz1, wyraz2, wyraz3;
-	int t1[2] = {0,0};
-	int t2[2] = {0,0};
-	int t3[2] = {0,0};
+	string wyraz1, wyraz2;
+	int t1[3] = {0,0,0};
+	int t2[3] = {0,0,0};
 	cout<<"wyraz 1: "<<endl;
 	cin>>wyraz1;
 	cout<<"wyraz 2: "<<endl;
 	cin>>wyraz2;
+
 	int d1 = wyraz1.size();
 	int d2 = wyraz2.size();
+	int d3 = wyraz3.size();
   	if(d1==d2) {
 	cout<<"Sprawdzamy liczbe kolejnych znakow"<<endl;
 	for(int i=0; i<d1; i++) {
 		if(wyraz1[i]=='x'){
 			t1[0]++;
 		
+		} else if(wyraz1[i]!='x') {
+			t1[1]++;
 		} else {
-			t1[1]++
+			t1[2]++;
 		}
 	if(wyraz2[i]=='x'){
 			t2[0]++;
@@ -32,5 +35,6 @@ int main() {
 		}
 	}
 }
+
 
 }
